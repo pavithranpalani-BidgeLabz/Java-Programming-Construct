028e879cc9e7208247347a1f53e5540c87303b96
pavithranpalani-bridgeLabz <pavithran.palani@bridgelabz.com>
2026-01-30 14:25:44 +0530

    day5 problem1

---
+++ src/main/java/org/example/D5P1.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Scanner;
+
+public  class D5P1{
+    public  void fibonacci(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of fibonacci series: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+    }
+}
+
